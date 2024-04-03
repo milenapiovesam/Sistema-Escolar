7520f64bb9f3a44dc896add76fc5f3f0e38ea42a
Milena Piovesam <milepiovesan2@gmail.com>
2024-04-03 10:38:16 -0300

    correção das funções já existentes; adição do loop que exibe os erros de preenchimento do formulário.

--- adicionar-novo-aluno.js
+++ adicionar-novo-aluno.js
@@ -1,21 +1,39 @@
 document.querySelector("#botao").addEventListener("click", function(event){ // O evento deve ser uma string.
     event.preventDefault();
 
+    var listaErros = document.getElementsByClassName("lista-erros")[0];
+    listaErros.innerHTML = "";
+
     var formulario = document.querySelector("form");
 
     let aluno = criarObjetoAluno()
 
-    let alunoLinha = criarLinhaAluno(aluno)
+    nomeValido = validarNome(aluno.nome);
+    nota1Valida = validarNota1(aluno.nota1);
+    nota2Valida = validarNota2(aluno.nota2);
 
-    // nomeValido = validarNome(aluno.nome);
-    // nota1Valida = validarNota1(aluno.nota1);
-    // nota2Valida = validarNota2(aluno.nota2);
+    
+    let erros = validarPaciente();
+
+    if (nomeValido && nota1Valida && nota2Valida) {
+        criarLinhaAluno(aluno);
+    }else {
+        for(i=0; i < erros.length; i++) {
+
+            let listaErros = document.getElementsByClassName("lista-erros")[0];
+            let itemErro = document.createElement("li");
+
+            itemErro.classList.add("erro")
+            itemErro.textContent = erros[i];
+            listaErros.appendChild(itemErro);
+        }
+    }
 
     // if (nomeValido && nota1Valida && nota2Valida) {
 
-        let tabela = document.getElementById("tabela-alunos");
+    // let tabela = document.getElementById("tabela-alunos");
 
-        tabela.appendChild(alunoLinha)
+    // tabela.appendChild(alunoLinha)
     // } 
 
 })
@@ -43,7 +61,7 @@ function criarObjetoAluno() {
 function criarLinhaAluno(aluno) {
 
     //Sempre começar pegando os elementos HTML já existentes necessários.
-    // let tabela = document.getElementById("tabela-alunos");
+    let tabela = document.getElementById("tabela-alunos");
 
     //Criar uma linha que irá receber as células/propriedades.
     let alunoLinha = document.createElement("tr");
@@ -55,6 +73,7 @@ function criarLinhaAluno(aluno) {
         celula.textContent = value; // Definir o texto da célula como o valor da propriedade
         alunoLinha.appendChild(celula); 
         
+        tabela.appendChild(alunoLinha)
     });
 
     // tabela.appendChild(alunoLinha)
@@ -67,7 +86,7 @@ function validarNota1 (nota1) {
         nota1Valida = true;
     } else {
         nota1Valida = false;
-        console.log("Primeira nota inválida");
+        // console.log("Primeira nota inválida");
     }
     return nota1Valida
 }
@@ -78,7 +97,7 @@ function validarNota2 (nota2) {
        nota2Valida = true;
     } else {
        nota2Valida = false;
-        console.log("Segunda nota inválida");
+        // console.log("Segunda nota inválida");
     } 
     return nota2Valida
     
@@ -90,9 +109,26 @@ function validarNome (nome) {
         nomeValido = true;
     }else {
         nomeValido = false;
-        console.log("Nome inválido.")
+        // console.log("Nome inválido.")
     }
     return nomeValido; 
 
 }
 
+function validarPaciente () {
+
+    let erros = []
+
+    if(!nomeValido){
+        erros.push("Nome inválido.")
+    } 
+    if (!nota1Valida){
+        erros.push("Primeira nota inválida.");
+    }
+    if (!nota2Valida) {
+        erros.push("Segunda nota inválida");
+    }
+
+    return erros
+
+}
